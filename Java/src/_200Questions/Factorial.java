14c1a82c1a4aea40c071b1d6dc94742b5d7dd306
varun <varunpatil316@gmail.com>
2025-05-03 16:17:56 +0530

    New Changes

--- Java/src/_200Questions/Factorial.java
+++ Java/src/_200Questions/Factorial.java
@@ -1,36 +1,20 @@
 package _200Questions;
-
 import java.util.Scanner;
-
 public class Factorial {
-
-	public static void main(String[] args) {
-	
-		// 4*3*2*1
-		
+	public static void main(String[] args) {	
+		// 4*3*2*1		
 		int fact = 1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Number to calculate factorial");
-		int numFact = sc.nextInt();
-		
+		int numFact = sc.nextInt();		
 		if (numFact<1) {
 			System.out.println("Factorial is " +fact);
-		}
-		
-		else {
-			
+		}	
+		else {			
 			for (int i=1; i<=numFact;i++) {
 				fact=fact*i;	
 			}
-			System.out.println(fact);
-			
-			
+			System.out.println(fact);	
 		}
-		
-		
-		
-		
-
 	}
-
 }
